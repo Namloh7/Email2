5696027b9c60a7e0a3b94f6aa6fbd90ad5b5ca4a
zan_baklazan <125310811+Ytovon@users.noreply.github.com>
2024-08-24 09:11:03 +0200

    first commit

--- Email/Email/Program.cs
+++ Email/Email/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Email;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,16 +18,30 @@ namespace Email
             string[] parts = email.Split('@');
             string ndPart = parts[1];
             string stPart = parts[0];
+
+            int intnum = 0;
+
             //číslo
-            string number = Regex.Replace(email, @"\D", "");
-            int intnum = Convert.ToInt32(number);
+            bool emaiContainsDigit = false;
+            string[] digits = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
 
-            
-           
+            foreach (string digit in digits)
+            {
+                emaiContainsDigit = email.Contains(digit) ? true : false;
+            };
 
-           //Křestní jméno, příjmeňí
-            
-            string[] names = stPart.Split('.', '1','2', '3', '4', '5', '6', '7', '8', '9', '0'); 
+
+            // přiřadí hodnotu číslu jen v případě, že email nějaké číslo obsahuje
+            if (emaiContainsDigit)
+            {
+                string number = Regex.Replace(email, @"\D", "");
+                intnum = int.Parse(number);
+            }
+
+            //Křestní jméno, příjmeňí
+            // Tady ti ještě chybí ty další oddělovače (- a _) jinak úplně v pořádku
+
+            string[] names = stPart.Split('.', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
             string name = names[0];
             string surname = names[1];
 
@@ -34,53 +49,40 @@ namespace Email
             string[] DomainMS = ndPart.Split('.');
             string mailserver = DomainMS[0];
             string Domain = DomainMS[1];
-            
-            
+
             Thread.Sleep(25);
-            Console.Write("Your name: "); SlowRead(name);
-            Console.Write("Your surname: "); SlowRead(surname);
-            Console.Write("Mailserver: "); SlowRead(mailserver);
-            Console.Write("Domain: "); SlowRead(Domain);
-            if (NumCheck(intnum) != -1) 
-                Thread.Sleep(25);
-                Console.Write("The year you were born: " + intnum);
-            Console.WriteLine();
-            int Age = 2024 - intnum;
-                Console.Write("Your age: " + Age );
+            Console.Write("Your name: "); HelperClass.SlowRead(name);
+            Console.Write("Your surname: "); HelperClass.SlowRead(surname);
 
-
-            Console.Read();
-
-        }
-        static int NumCheck(int number)
-        {
-            if (number >= 1899 && number <= 2019)
+            if (NumCheck(intnum, out int Age))
             {
-                int Age = 2024 - number;
-                return Age;
+                Thread.Sleep(25);
+                Console.WriteLine("The year you were born: " + intnum);
+                Console.WriteLine("Your age: " + Age);
+            }
+
+            Console.Write("Mailserver: "); HelperClass.SlowRead(mailserver);
+            Console.Write("Domain: "); HelperClass.SlowRead(Domain);
+
+            Console.ReadLine();
+        }
+
+        static bool NumCheck(int number, out int Age)
+        {
+            bool success = true;
+
+            // tady jsem jen ty datumy chtěl dynamické pomocí DateTime třídy... 
+            if (number >= DateTime.Now.Year - 125 && number <= DateTime.Now.Year - 5)
+            {
+                Age = DateTime.Now.Year - number;
             }
             else
             {
-                return -1;
+                Age = 0;
+                success = false;
             }
-
-
-
-
+            return success;
         }
-        static void  SlowRead(string s) 
-        {
-            
-            foreach (char c in s)
-            {
-                Thread.Sleep(25);
-                Console.Write(c);
-            }
-            Console.WriteLine();
-        }
-        
-
 
     }
-    
 }
