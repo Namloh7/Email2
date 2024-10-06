265b7bc0bf2f776f33cde6df290fd59db865e8d3
Namloh7 <namloh321@gmail.com>
2024-10-06 18:56:01 +0200

    Add files via upload

--- EMAILS.cs
+++ EMAILS.cs
@@ -17,87 +17,20 @@ namespace Email
     {
         static void Main(string[] args)
         {
-        List<Person> persons = new List<Person>();
-        List<string> emails = new List<string>();
-             
-            string currentDirectory = "Emaily.txt";
-
-            emails = File.ReadAllLines(currentDirectory).ToList();
+            Person.Peoples();
 
 
 
-            foreach (string email in emails)
-            {
-
-
-
-                string[] parts = email.Split('@');
-                string ndPart = parts[1];
-                string stPart = parts[0];
-
-                string number = Regex.Replace(email, @"\D", "");
-                int intnum;
-                bool hasValidNumber = int.TryParse(number, out intnum);
-
-                string[] names = stPart.Split('.', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
-                string name = names[0];
-                string surname = names[1];
-
-                string[] DomainMS = ndPart.Split('.');
-                string mailserver = DomainMS[0];
-                string domain = DomainMS[1];
-
-                if (hasValidNumber && NumCheck(intnum) != -1)
-                {
-                    int age = 2024 - intnum;
-                    Person person = new Person(name, surname, mailserver, domain, age);
-                    persons.Add(person);
-                }
-                else 
-                {
-                    Person person = new Person(name, surname, mailserver, domain);
-                    persons.Add(person);
-                }
-
-
-
-                
-            }
-            int i = 0;
-            foreach(Person p in persons) 
-            {
-                Console.WriteLine("Your name. " + p.Name);
-                Console.WriteLine("Your surname. " + p.Surname);
-                Console.WriteLine("Your mailserver: " + p.Mailserver);
-                Console.WriteLine("Your domain: " + p.Domain);
-                if (p.Age != null) 
-                {
-                    Console.WriteLine("Your age: " + p.Age);
-                }
-                Console.WriteLine("++++++++++++++++++++++++++++++++++++++");
-
-            }
+            
+            
+            
+            
 
             Console.Read();
 
 
         }
-        static int NumCheck(int number)
-        {
-            if (number >= 1899 && number <= 2019)
-            {
-                int Age = 2024 - number;
-                return Age;
-            }
-            else
-            {
-                return -1;
-            }
-
-
-
-
-        }
+        
 
     }
         
--- Person.cs
+++ Person.cs
@@ -19,28 +19,78 @@ namespace EMAILS
         public int? Age;
 
 
-        public Person(string name,string surname,string mailserver,string domain,int? age = null)
+        public Person(string name, string surname, string mailserver, string domain, int? age = null)
         {
             this.Name = name;
             this.Surname = surname;
             this.Mailserver = mailserver;
             this.Domain = domain;
             this.Age = age;
-            
-            
 
-            
+
+
+
 
 
         }
-        
+        public static void Peoples()
+        {
+            List<Person> persons = new List<Person>();
+            string currentDirectory = "Emaily.txt";
+            List<string> emails = new List<string>();
+            emails = File.ReadAllLines(currentDirectory).ToList();
+
+            foreach (string email in emails)
+            {
+                string[] parts = email.Split('@');
+                string ndPart = parts[1];
+                string stPart = parts[0];
+
+                string number = Regex.Replace(email, @"\D", "");
+
+                bool hasValidNumber = int.TryParse(number, out int intnum);
+
+                string[] names = stPart.Split('.', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
+                string name = names[0];
+                string surname = names[1];
+
+                string[] DomainMS = ndPart.Split('.');
+                string mailserver = DomainMS[0];
+                string domain = DomainMS[1];
+
+                if (hasValidNumber && intnum >= DateTime.Now.Year - 100 && intnum <= DateTime.Now.Year)
+                { 
+                
+                int age = DateTime.Now.Year - intnum;
+                    Person person = new Person(name, surname, mailserver, domain, age);
+                    persons.Add(person);
+                }else 
+                {
+                    Person person = new Person(name, surname, mailserver, domain);
+                    persons.Add(person);
+                }
+                foreach (Person p in persons)
+                {
+                    Console.WriteLine("Your name. " + p.Name);
+                    Console.WriteLine("Your surname. " + p.Surname);
+                    Console.WriteLine("Your mailserver: " + p.Mailserver);
+                    Console.WriteLine("Your domain: " + p.Domain);
+                    if (p.Age != null)
+                    {
+                        Console.WriteLine("Your age: " + p.Age);
+                    }
+                    Console.WriteLine("++++++++++++++++++++++++++++++++++++++");
+
+                }
+
+            }
 
 
 
 
-        
-        
 
+
+
+        }
     }
 }
-
