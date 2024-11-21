2361aefaff8b98fb526d2d06cabb49db25a19916
Namloh7 <namloh321@gmail.com>
2024-11-21 21:54:49 +0100

    Add files via upload

---
+++ Assigment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoClass
+{
+    public class Assigment
+    {
+        public string content { get; set; }
+        public bool isfavourite {  get; set; }
+        public Assigment(string con) 
+        {
+            content = con;
+            isfavourite = false;
+
+         
+            
+        }  
+        public void Favourite() 
+        { 
+            this.isfavourite = true;
+        }
+        public void UnFavourite() 
+        {
+            this.isfavourite = false;
+        }
+    }
+}
--- HelperClass.cs
+++ HelperClass.cs
@@ -8,20 +8,27 @@ namespace ToDoClass
 {
     public static class HelperClass
     {
-    
-    public static int GetIntInRange(string message, int min, int max) 
+
+        public static int GetIntInRange(string message, int min, int max)
+        {
+            bool isnumber = true;
+            int value = 0;
+            do
             {
-                bool isnumber = true;
-                int value = 0;
-                do
-                {
-                    Console.WriteLine(message);
-                    isnumber = int.TryParse(Console.ReadLine(), out value);
-                }
-                while (value < min && value > max && !isnumber);
-                return value;
+                Console.WriteLine(message);
+                isnumber = int.TryParse(Console.ReadLine(), out value);
             }
+            while (value <= min && value >= max && !isnumber);
+            return value;
+        }
+        public static void WriteLine(string message)
+        {
+            Console.WriteLine(message);
+        }
 
-
+        public static string ReadLine() 
+        { 
+        return Console.ReadLine();
+        }
     }
 }
